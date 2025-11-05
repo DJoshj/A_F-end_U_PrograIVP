539ac3dbd7e039aae75dec7e546bbf45afaf7083
Jose Alberto Rodriguez <josealbertorodriguez2011@gmail.com>
2025-11-04 18:22:50 -0600

    LoginAuth1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { Home } from './components/home/home';
 import { Estudiantes } from './components/estudiantes/estudiantes';
 import { Profile } from './components/profile/profile';
 import { MostrarEstudiantes } from './components/estudiantes/sub-component/mostrar-estudiantes/mostrar-estudiantes';
+import { Administracion } from './components/administracion/administracion';
 
 export const routes: Routes = [
     {path: 'login', component: Login},
     {path:'home',component:Home,children:[
         {path:'estudiantes',component:Estudiantes, children:[{path:'mostrarEstudiantes',component:MostrarEstudiantes}]},
         {path:'profile',component:Profile},
+        {path:'administracion',component:Administracion}
     ]
 
     },
